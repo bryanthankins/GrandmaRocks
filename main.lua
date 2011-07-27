96bda9699a4fa6db9d568f1fe1cf46c5617fbd32
Bryant Hankins <bryanthankins@gmail.com>
2011-07-27 16:06:21 -0500

    added KO and explosion anims

--- main.lua
+++ main.lua
@@ -2,14 +2,17 @@
 ---By Bryant Hankins
 
 --TODO
---1) Move Grandma back and forth
---3) Do Multiple levels
---6) Add collision and explosion graphics
+--3) Do Multiple levels (see ghosts vs zombies)
+--6) Add collision and explosion graphics (see movieclip or eggbreaker)
+--8) Add splash screen
 
 
 --Set up physics
 local physics = require("physics")
 physics.start()
+
+--Used for anim
+local movieclip = require( "movieclip" )
 
 --UI used for scoreboard
 local ui = require( "ui" )
@@ -17,13 +20,15 @@ local ui = require( "ui" )
 --Set background
 display.setStatusBar( display.HiddenStatusBar )
 local background = display.newImage("sky.png", true)
-local grass = display.newImage( "grandma.png", true )
+--local grass = display.newImage( "grandma.png", true )
+local grass = movieclip.newAnim{ "grandma.png", "grandma_ko.png" }
 grass.x = 160
 grass.y = 440
 physics.addBody( grass, "static", { friction=0.5, bounce=0.3 } )
 
 --Load sounds
-local squishSound = audio.loadSound("bomb_wav.wav")
+local bombSound = audio.loadSound("bomb_wav.wav")
+local squishSound = audio.loadSound("squish_wav.wav")
 
 --Set instructions
 local instructionLabel = display.newText( "Don't let the rocks hit grandma!", 22, 20, native.systemFont, 17 )
@@ -44,14 +49,16 @@ scoreDisplay:setText( score )
 
 ------------------------------------------------------------
 
--- A touch callback to remove balloons
+-- A touch callback to remove falling items
 local removeBody = function( event )
 	local t = event.target
 	local phase = event.phase
 
 	if "began" == phase then
-        audio.play( squishSound )				
-		t:removeSelf() -- destroy object
+        --t:stopAtFrame(2)
+		t:play{ startFrame=7, endFrame=15, loop=1, remove=true } 
+        audio.play( bombSound )				
+		--t:removeSelf() -- destroy object
         -- Update Score
         score = score + 150
         scoreDisplay:setText( score )
@@ -61,6 +68,22 @@ local removeBody = function( event )
 	return true
 end
 
+local function hitGrandma (self, event )
+	local phase = event.phase
+
+	if "began" == phase then
+        self:stopAtFrame(2)
+		--t:play{ startFrame=7, endFrame=15, loop=1, remove=true } 
+        audio.play( squishSound )				
+		--t:removeSelf() -- destroy object
+        -- Update Score
+        --score = score + 150
+        --scoreDisplay:setText( score )
+        local gameOverLabel = display.newText( "GAME OVER!", 22, 40, native.systemFont, 40 )
+	end
+
+	-- Stop further propagation of touch event
+end
 local balls = {}
 
 -- function to drop rocks
@@ -69,7 +92,17 @@ local randomBall = function()
 	choice = math.random( 100 )
 	local ball
 	
-    ball = display.newImage( "rock.png" )
+    --ball = display.newImage( "rock.png" )
+	imageTable = {}
+	for i = 1,6 do
+		table.insert( imageTable, "cube" .. i .. ".png" )
+	end
+	for i = 1,9 do
+		table.insert( imageTable, "explode" .. i .. ".png" )
+	end
+	
+	ball = movieclip.newAnim( imageTable )
+    --ball = movieclip.newAnim{ "rock.png", "egg_cracked.png" }
     ball.x = 40 + math.random( 380 ); ball.y = -40
     physics.addBody( ball, { density=2.0, friction=0.6, bounce=0.2, radius=33 } )
     ball.angularVelocity = math.random(600) - 300
@@ -78,6 +111,10 @@ local randomBall = function()
 	ball:addEventListener( "touch", removeBody ) -- assign touch listener to rock
 	balls[#balls + 1] = ball	
 end
+
+--Collision with grandma
+grass.collision = hitGrandma
+grass:addEventListener( "collision", grass )
 
 -- run the above function 14 times
 timer.performWithDelay( 1500, randomBall, 14 )
